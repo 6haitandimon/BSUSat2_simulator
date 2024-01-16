f4adedfa85fca464b5f3deb95e977795692f0f24
57459N <stasun008@gmail.com>
2024-01-16 09:49:16 +0300

    fixed stupid logic error

--- e10_433/tests/test_radio_receive.py
+++ e10_433/tests/test_radio_receive.py
@@ -4,10 +4,10 @@ from radio_driver.radio_controller import RadioController
 def test_radio_receive():
     controller = RadioController()
 
-    if not controller.initialize():
+    if controller.initialize():
         print("Test radio receive: failed to init controller")
         return
 
-    if not controller.start_rx():
+    if controller.start_rx():
         print(print("Test radio receive: failed to start rx"))
         return
