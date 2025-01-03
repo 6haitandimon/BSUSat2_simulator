4bc3ebb0cac97bfb6b90792c5a7f06c9c54e51f7
6haitandimon <borejko2017@gmail.com>
2025-01-03 19:54:04 +0300

    new driver

--- INTERRUPT/include/interrupt.h
+++ INTERRUPT/include/interrupt.h
@@ -1,15 +1,13 @@
-//#include "hardware/spi.h"
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "stdio.h"
 #include "MCP2515.h"
 #include "canTransmit.h"
-#include "rtc.h"
+
 #define MCPInterruptPin 22
 
 extern uint16_t MatherBoardTelemtry[62];
 extern MCP2515 can0;
-extern RTC::RTC rtc;
 extern uint32_t last_reset_time;
 union uint32Time{
     uint32_t data;
--- INTERRUPT/interrupt.cpp
+++ INTERRUPT/interrupt.cpp
@@ -1,42 +1,7 @@
 #include "interrupt.h"
 
 void canAnswer(can_frame rx){
-//    can0.clearTXInterrupts();
-    can_frame tx;
-    tx.can_id = 0x01;
-    tx.can_dlc = 1;
-    tx.data[0] = 62;
-    if(rx.data[1] == 9){
-        if(can0.sendMessage(&tx) ==MCP2515::ERROR_OK){
-            printf("Answer done\n");
-            printf("Count frame: %d\n", tx.data[0]);
-        }
-        busy_wait_ms(100);
-//        sleep_ms(2000);
-        uint32Time unixTimeData;
-        unixTimeData.data = rtc.dataTimeUnix() - last_reset_time;
 
-        MatherBoardTelemtry[12] = unixTimeData.data1;
-        MatherBoardTelemtry[13] = unixTimeData.data2;
-
-        unixTimeData.data = rtc.dataTimeUnix();
-        MatherBoardTelemtry[20] = unixTimeData.data1;
-        MatherBoardTelemtry[21] = unixTimeData.data2;
-
-        for(int index = 0; index < tx.data[0]; index++){
-            can_frame frame;
-            frame.can_dlc = 1;
-            frame.data[0] = index + 1;
-            CAN::CanPreparation(frame, 2, MatherBoardTelemtry[index]);
-            uint8_t error = -1;
-            while(error != MCP2515::ERROR::ERROR_OK){
-                error = can0.sendMessage(&frame);
-                busy_wait_ms(100);
-                }
-            printf("can frame number %d send done", index);
-        }
-    }
-    return;
 }
 
 void MCPCallback(uint gpio, uint32_t events){
